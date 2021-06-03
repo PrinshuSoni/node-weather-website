de40729d08a0cae10742fe1ff261ec689b2e65f2
Prinshu Soni <soni.prinshu@gmail.com>
2021-06-03 16:12:47 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client side javascript is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const locationMsg = document.querySelector('#location')
+const forecastMsg = document.querySelector('#forecast')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const location = search.value
+    
+    locationMsg.textContent = 'Loading...'
+    forecastMsg.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                locationMsg.textContent = data.error;
+            } else {
+                locationMsg.textContent = data.location;
+                forecastMsg.textContent = data.forecast;
+            }
+        })
+    })
+})
